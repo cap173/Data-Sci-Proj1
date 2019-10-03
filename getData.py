473ff3fe29285392a8fce029e788e7ea10cd60e8
Chris Paniagua <cap173@georgetown.edu>
2019-10-03 14:33:56 -0400

    Adding file with API key  that will extract data from API.

---
+++ getData.py
@@ -0,0 +1,15 @@
+import requests
+import csv
+import json
+
+endPoint = 'd'
+APIKey = 'ad6e0cbf8162ba419f0840b0dee78a59934e2bc1'
+
+
+
+def main():
+    print( APIKey )
+        
+        
+if __name__== "__main__" :
+    main()
