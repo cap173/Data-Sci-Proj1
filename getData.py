7e640b2a91ec099fa2e30dc477ffe8d74ba9289e
Chris Paniagua <cap173@georgetown.edu>
2019-10-05 16:07:25 -0400

    Addint code that requests both datasets from API.

--- getData.py
+++ getData.py
@@ -1,15 +1,24 @@
 import requests
 import csv
 import json
+import pandas as pd
 
-endPoint = 'd'
-APIKey = 'ad6e0cbf8162ba419f0840b0dee78a59934e2bc1'
-
+urlFor2000 = 'https://opendata.arcgis.com/datasets/f88d947d0ba945b688e00a46d6cbcd6c_9.csv'
+csv2000 = 'census_2000.csv'
+urlFor2010 = 'https://opendata.arcgis.com/datasets/6969dd63c5cb4d6aa32f15effb8311f3_8.csv'
+csv2010 = 'census_2010.csv'
 
 
 def main():
-    print( APIKey )
-        
+   request = requests.get( urlFor2000 )
+   outFile = open( csv2000, 'wt' )
+   outFile.write( request.text )
+   
+   request = requests.get( urlFor2010 )
+   outFile = open( csv2010, 'wt' )
+   outFile.write( request.text )
+   print( outFile )
+
         
 if __name__== "__main__" :
     main()
